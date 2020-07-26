dde4cfb8620b53f8ccf16357b6210d47b84e51ef
markgalante <mark.galante.14@gmail.com>
2020-07-26 15:48:08 +0200

    Updates

---
+++ precache-manifest.4b9a8d0e6d06dd8025a0a38f5f784c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b883f26df72359c2fcd6c744a9221c7",
+    "url": "/morning-bru/index.html"
+  },
+  {
+    "revision": "1ec948cb682cb1d4cdfd",
+    "url": "/morning-bru/static/css/main.f2c4bcd5.chunk.css"
+  },
+  {
+    "revision": "5dcf8ef4b6c36a54ecfb",
+    "url": "/morning-bru/static/js/2.e31e8101.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/morning-bru/static/js/2.e31e8101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec948cb682cb1d4cdfd",
+    "url": "/morning-bru/static/js/main.c5cee32b.chunk.js"
+  },
+  {
+    "revision": "90a18e1b7e57b208f1be",
+    "url": "/morning-bru/static/js/runtime-main.2893ef8e.js"
+  }
+]);
